0ceba5386438f582cdfe73ed358b92df857e6dd1
Superville <steve.superville.gg@gmail.com>
2019-05-30 17:54:09 -0400

    Enable GameplayAbilitySystem

--- KFBB/Source/KFBB/KFBB.Build.cs
+++ KFBB/Source/KFBB/KFBB.Build.cs
@@ -8,7 +8,7 @@ public class KFBB : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", "Core", "CoreUObject", "Engine", "InputCore", "UMG", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
